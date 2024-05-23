a6ad45618de4b3fbf237bfc5973f613a19bd62b0
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-05-23 12:55:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+  export default {
+    components: {
+      HelloWorld 
+    },
+    data() {
+      return {
+        message: "hello vite!"
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+      console.log("msg di saluto")
+    }
+  }
+</script>
+
+<template>
+  <h1>Hello Vite!</h1>
+</template>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+  data() {
+    return {
+      msg: "Ciao a tutti!"
+    }
+  }
+}
+</script>
+
+<template>
+    <h2>Ciao a tutti!</h2>
+</template>
+
+
